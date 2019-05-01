84c70dc938f6c027700680fb5671c2b7775f6301
sadunalpdag <49690949+sadunalpdag@users.noreply.github.com>
2019-05-01 16:07:58 +0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,65 @@
+a=10
+if a>20:
+    print ("true statemet")
+else:
+    print ("false statement")
+
+if True:
+    print ("true statement")
+
+print("ratan"),print("ratana") if 10>20 else print ("durga"),print("durgara")
+
+{print("ratan"),print("ratana")} if 10>20 else {print ("durga"),print("durgara")}
+
+a=10
+
+if a==10:
+    print("true statement")
+elif a==20:
+    print("false statement1")
+else:
+    print("false stateement2")
+
+
+
+for data in range(10):
+
+    print(data)
+
+
+
+for data in range(2,10):
+    print(data)
+
+for data in range(2,10,5):
+    print(data)
+
+L=[10,20,30]
+
+for x in L:
+    print(x)
+
+t=(10,20,30)
+for x in t:
+    print(x)
+
+for x in range(10):
+    print (x)
+    print("normal")
+else:
+    print ("normal")
+
+for x in range(10):
+   if x==3:
+        break
+        print(x)
+else:
+    print("dkamls")
+
+import os
+
+for x in range(-10,10):
+    print(x)
+    os.exit(0)
+else:
+    print("normal execution")
